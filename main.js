cc4c0212f8aaf004133f38bbf9a92d1cdc2673b1
Itay Neuberger <itayneu@gmail.com>
2022-05-20 13:59:24 +0300

    feat: init game

--- main.js
+++ main.js
@@ -1,49 +1,144 @@
 class AsyncGame {
-    constructor() {
-        this.API_BASE = 'https://u-workshops.herokuapp.com'
-    }
+  constructor() {
+    this.API_BASE = "https://u-workshops.herokuapp.com";
+    this.userId;
+  }
 
-    /* 
+  /* 
         Note: most of these methods will use the `fetch` API
         It's ok if you don't fully understand it yet! You can think of it as a 'blackbox' for now
     */
 
-    async createUser() {
-        // POST request to the /new_user endpoint
+  async createUser(name) {
+    // POST request to the /new_user endpoint
+    try {
+      const response = await fetch(`${this.API_BASE}/new_user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+        }),
+      });
+      const result = await response.json();
+      console.log(result);
+      this.userId = result.userId;
+    } catch (error) {
+      // enter your logic for when there is an error (ex. error toast)
+      console.log(error);
     }
+  }
 
-    async addToQABank() {
-        // POST request to /new_qa
+  async addToQABank(question, answer) {
+    // POST request to /new_qa
+    // {ownerId: YOUR_USER_ID, question: "STRING QUESTION", answer: "STRING OR INT ANSWER"}
+    try {
+      const response = await fetch(`${this.API_BASE}/new_qa`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ownerId: this.userId,
+          question: question,
+          answer: answer,
+        }),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      // enter your logic for when there is an error (ex. error toast)
+      console.log(error);
     }
+  }
 
-    async getAllQuestions() {
-        // GET request to /all_questions
-        // Note! More questions will be added as other students progress in this workshop.
-        // Ask around to see who's added new questions!
+  async getAllQuestions() {
+    // GET request to /all_questions
+    // Note! More questions will be added as other students progress in this workshop.
+    // Ask around to see who's added new questions!
+    try {
+      const response = await fetch(`${this.API_BASE}/all_questions`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      // enter your logic for when there is an error (ex. error toast)
+      console.log(error);
     }
+  }
 
-    async answerQuestion() {
-        // POST request to /answer_question
-        // Note! In the response of this request you'll see whether your answer was correct or not.
-        // If you answered incorrectly, try again or bring it up with the user who posted the question!
+  async answerQuestion(qaId, answer) {
+    // POST request to /answer_question
+    // Note! In the response of this request you'll see whether your answer was correct or not.
+    // If you answered incorrectly, try again or bring it up with the user who posted the question!
+    // game.answerQuestion({qaId: ID_FROM_SERVER, answer: YOUR_ANSWER, userId: YOUR_USER_ID})
+    try {
+      const response = await fetch(`${this.API_BASE}/answer_question`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: this.userId,
+          qaId: qaId,
+          answer: answer,
+        }),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      // enter your logic for when there is an error (ex. error toast)
+      console.log(error);
     }
+  }
 
-    async getAnswerSubmissions() {
-        // GET request to /answer_submissions
+  async getAnswerSubmissions() {
+    // GET request to /answer_submissions
+    try {
+      const response = await fetch(`${this.API_BASE}/answer_submission`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      // enter your logic for when there is an error (ex. error toast)
+      console.log(error.message);
     }
+  }
 
-    async getUsers(){
-        // GET request to /the_users
+  async getUsers() {
+    // GET request to /the_users
+    try {
+      const response = await fetch(`${this.API_BASE}/the_users`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      // enter your logic for when there is an error (ex. error toast)
+      console.log(error);
     }
+  }
 
-    async calculateUserScores() {
-        // +1 points for questions you've answered correctly
-        // -1 points for questions you've answered incorrectly
-
-        // This is the most "complicated" method - but you've got this ;)
-
-        // Guidelines for this part (ignore if you want an extra challenge!)
-        /*
+  async calculateUserScores() {
+    // +1 points for questions you've answered correctly
+    // -1 points for questions you've answered incorrectly
+    // This is the most "complicated" method - but you've got this ;)
+    // Guidelines for this part (ignore if you want an extra challenge!)
+    /*
             - Get the users
             - Get the submissions
             - Create an `scores` object
@@ -59,15 +154,21 @@ class AsyncGame {
                 Darwin: -1
             }
         */
-    }
+    let users = this.getUsers();
+    let submissions = this.getAnswerSubmissions();
+    let scores = {};
+
+    users.forEach((index, user) => {
+      console.log(index, user);
+      let username = user.name;
+    });
+  }
 }
 
-const game = new AsyncGame()
+const game = new AsyncGame();
 // Remember the server is unexpected, it might return an error!
 
 // Example of running the game:
-// game.createUser("Frank")
-// game.addToQABank({question: "How many legs does a cat have?", answer: 4, ownerId: YOUR_USER_ID})
 
 // game.getAllQuestions()
 // game.answerQuestion({qaId: ID_FROM_SERVER, answer: YOUR_ANSWER, userId: YOUR_USER_ID})
